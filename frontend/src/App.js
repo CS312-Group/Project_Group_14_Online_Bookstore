8d12b9d62a4d67386188a6375c5cc0aa9a7b64bb
Joshua <jcv234@nau.edu>
2024-11-21 17:51:48 -0700

    JOSH: add redirect to signin from home
    Signed-off-by: Joshua <jcv234@nau.edu>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,14 @@ import Signup from './pages/signup';
 import Signin from './pages/signin';
 import Books from './pages/books';
 import Reviews from './pages/reviews';
+import Home from "./pages/home";
 
 const App = () => {
     return (
         <Router>
             <div>
                 <Routes>
+                    <Route path="/" element={<Home />} />
                     <Route path="/signup" element={<Signup />} />
                     <Route path="/signin" element={<Signin />} />
                     <Route path="/books" element={<Books />} />
